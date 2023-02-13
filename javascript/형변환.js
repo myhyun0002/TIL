3530ee5689517f024b8b86a890bae258c2651ad0
myhyun0002 <a01089417552@gmail.com>
2023-02-13 19:08:23 +0900

    [feat] #13 index.html 파일 생성

--- javascript/형변환.js
+++ javascript/형변환.js
@@ -2,6 +2,7 @@ String()    // 문자형으로 변환
 Number()    // 숫자형으로 변환
 Boolean()   // Boolean 형으로 변환
 
+
 const mathScore = prompt("수학 몇점?");
 const engScore = prompt("영어 몇점?");
 const result = mathScore + engScore / 2;
