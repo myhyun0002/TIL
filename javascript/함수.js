fce6c1c2b239c525661eb56f55e99d30fc0a9afa
myhyun0002 <a01089417552@gmail.com>
2023-02-16 12:34:32 +0900

    [fix] #13 함수 정리 수정

--- javascript/함수.js
+++ javascript/함수.js
@@ -7,17 +7,21 @@
 
 // 매개변수가 있는 함수
 function sayHello(name) {
-  console.log(`Hello,${name}`);
+  let msg = 'Hello';
+  if(name){
+    msg += `, ${name}`;
+  }
+  console.log(msg);
 }
 
 sayHello("이름");
 
 
 
-
 // 매개변수가 없는 함수
 function showError() {
-  console.log(`에러가 발생했습니다.. 다시 시해세요`);
+  console.log(`에러가 발생했습니다.. 다시 시도해주세요`);
 }
 
 showError();
+
