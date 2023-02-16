afdad2a34b2bef55af63688bb8712149d1b3ca79
myhyun0002 <a01089417552@gmail.com>
2023-02-16 12:31:41 +0900

    [feat] #13 함수 정리

--- javascript/함수.js
+++ javascript/함수.js
@@ -3,8 +3,21 @@
 
 */
 
-function sayHello(name){
+
+
+// 매개변수가 있는 함수
+function sayHello(name) {
   console.log(`Hello,${name}`);
 }
 
 sayHello("이름");
+
+
+
+
+// 매개변수가 없는 함수
+function showError() {
+  console.log(`에러가 발생했습니다.. 다시 시해세요`);
+}
+
+showError();
