7ea798be1ba6ceef09943cd7b6d6c7fabb4c0dc9
myhyun0002 <a01089417552@gmail.com>
2023-03-10 17:58:20 +0900

    [feat] #17 10988

---
+++ algorithm/baekjoon/10988.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+/*
+  stack
+  
+
+*/
+
+int main(){
+  string str;
+  cin >> str;
+
+  stack<char> s;
+
+  int str_half_size = str.size() / 2;
+
+  for(int i = 0; i < str_half_size; i++){
+    s.push(str[i]);
+  }
+
+  int result = 1;
+
+  int search_start_index;
+
+  if(str.size() % 2 == 0){
+    search_start_index = str_half_size;
+  }else{
+    search_start_index = str_half_size + 1;
+  }
+
+  for(int i = search_start_index ; i < str.size() ; i++){
+    char current_str = s.top();
+    s.pop();
+    if(current_str != str[i]){
+      result = 0;
+      break;
+    }
+  }
+
+  cout << result << endl;
+  return 0;
+}
